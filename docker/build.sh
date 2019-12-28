05b89ea1a83a00626247fa1cf54b77acf1b48b8d
wangh09 <wanghaoforever@126.com>
2019-12-28 13:55:28 +0800

    add csv

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd ../ && ./gradlew build && cd docker
 cp ../build/libs/*.jar ./app.jar
-docker build -t bootapp/dal-core .
-docker push bootapp/dal-core
+docker login --username=bootapp@1044358033708111 -pabcd1234 registry.cn-beijing.aliyuncs.com
+docker build -t registry.cn-beijing.aliyuncs.com/bootapp/dal-core .
+docker push registry.cn-beijing.aliyuncs.com/bootapp/dal-core
